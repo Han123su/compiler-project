61288663c25b027103306a792eaea6ad83f068bb
Han123su <107395380+Han123su@users.noreply.github.com>
2022-06-13 21:17:50 +0800

    Update gcc-action.sh

--- gcc-action.sh
+++ gcc-action.sh
@@ -15,40 +15,10 @@ yacc yacc.y
 yacc -d yacc.y
 cc lex.yy.c y.tab.c -o compiler1
 
-echo
-echo "-------------------------sample1.c-------------------------"
-./compiler1 < sample1.c
-
-echo
-echo "-------------------------sample2.c-------------------------"
-./compiler1 < sample2.c
-
-echo
-echo "-------------------------sample3.c-------------------------"
-./compiler1 < sample3.c
-
-echo
-echo "-------------------------sample4.c-------------------------"
-./compiler1 < sample4.c
-
-echo
-echo "-------------------------sample5.c-------------------------"
-./compiler1 < sample5.c
-
-echo
-echo "-------------------------sample6.c-------------------------"
-./compiler1 < sample6.c
-
-echo
-echo "-------------------------sample7.c-------------------------"
-./compiler1 < sample7.c
-
-echo
-echo "-------------------------My program-------------------------"
-./compiler1 < mytest.c
-
 sudo apt-get update && sudo apt-get install gcc
-gcc -S -masm=intel sample3.c -o ass_intel
+gcc -S -masm=intel mytest.c -o ass_intel
+
+
 echo
-echo "(gcc) assembly language:"
+echo "---------------- assembly language (gcc) --------------------"
 cat ass_intel
