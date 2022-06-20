ee992df38ce251b4d3287defd1e828c0c714aace
Han123su <107395380+Han123su@users.noreply.github.com>
2022-06-20 20:36:32 +0800

    Update mytest.c

--- mytest.c
+++ mytest.c
@@ -1,37 +1,22 @@
-#include<bits/stdc++.h>
-using namespace std;
-void perm(char[],int,int); 
 int main()
 {
-	int m;  
-	printf("輸入共有幾個字元排序:");
-	scanf("%d",&m);
-	
-	int i=0,n=strlen(list)-1; 
-	printf("輸入字元(不須空格):");
-	for(int k=-1;k<=m;k++)
-	{
-		scanf("%c",&list[k]);
-	}
-	
-	perm(list,i,n); 
+    int i=5;
+    void palin(int n);
+    printf("请输入5个字符\40:\40");
+    palin(i);
+    printf("\n");
 }
-
-void perm(char list[],int i,int n)    //遞迴   
-{     
-    if(i==n)
-    {
-    	for(int j=0;j<=n;j++)
-    		printf("%c",list[j]);
-    	printf("\n");
-	}
-	else
-    {
-    	for(int j=i;j<=n;j++)
-		{
-			swap(list[i],list[j]);
-			perm(list,i+1,n);
-			swap(list[i],list[j]);	
-		}   
-	}
+void palin(n)
+int n;
+{
+    char next;
+    if(n<=1) {
+        next=getchar();
+        printf("相反顺序输出结果\40:\40");
+        putchar(next);
+    } else {
+        next=getchar();
+        palin(n-1);
+        putchar(next);
+    }
 }
